72a90f41d06fc2f73f0f5dc311a3675bb6c06ab1
DayaneArnaud <dayanearnaud@gmail.com>
2024-04-01 16:34:37 -0300

    mongo and mongoose configuration and connection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
+const dbURI = process.env.DB_URI as string;
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("database started");
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  return res.json("Tudo certo");
-});
-
-app.listen(process.env.PORT, () => {
-  console.log("Server started");
-});
+    const app = express();
+    app.use(express.json());
+    app.get("/", (req, res) => {
+      return res.json("Tudo certo");
+    });
+    app.listen(process.env.PORT, () => {
+      console.log("Server started");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
